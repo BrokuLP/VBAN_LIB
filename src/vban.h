e80ae727ee769b238cb06b930db5e0bc1897a945
BrokuLP <86626273+BrokuLP@users.noreply.github.com>
2024-03-01 11:27:46 +0100

    added cpp and header file

---
+++ src/vban.h
@@ -0,0 +1,21 @@
+/**
+ * header file for vban library
+*/
+
+class vban
+{
+private:
+    void _decodeHeader();
+    void _handleProtAudio();
+public:
+    vban(/* args */);
+    ~vban();
+};
+
+vban::vban(/* args */)
+{
+}
+
+vban::~vban()
+{
+}
